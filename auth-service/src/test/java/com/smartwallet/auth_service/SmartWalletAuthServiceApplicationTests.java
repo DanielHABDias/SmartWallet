8d71e8a0d43c5b2d18a83c1ba948b9ac5265ff4c
Daniel Henrique Alves Bicalho Dias <daniel.dias@4mti.com.br>
2025-09-18 10:32:22 -0300

    #0001 - New Develop: Inicialização do projeto.

---
+++ auth-service/src/test/java/com/smartwallet/auth_service/SmartWalletAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartwallet.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWalletAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
